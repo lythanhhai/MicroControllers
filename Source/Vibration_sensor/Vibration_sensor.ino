c011e9907d3db1d6cf1d8253eefe06d0ee19934a
lythanhhai <you@example.com>
2022-03-02 08:17:13 +0700

    test vibrate_sensor and fire_sensor with arduino

---
+++ Source/Vibration_sensor/Vibration_sensor.ino
@@ -0,0 +1,24 @@
+/*Vibration sensor tutorial.
+  created by the SriTu Tech team.
+  Read the code below and use it for any of your creation.
+  https://srituhobby.com
+*/
+
+#define LED 8
+#define Sensor 9
+void setup() {
+  Serial.begin(9600);
+  pinMode(Sensor, INPUT);
+  pinMode(LED, OUTPUT);
+}
+void loop() {
+//  bool value = digitalRead(Sensor);
+//  delay(100);
+//  Serial.print("Gia tri doc duoc la: ");
+//  Serial.println(value);
+//  if (value == 1) {
+//    digitalWrite(LED,HIGH);
+//  }else if(value == 0){
+//    digitalWrite(LED,LOW);
+//  }
+}
---
+++ Source/fire_sensor/fire_sensor.ino
@@ -0,0 +1,28 @@
+void setup() {
+  pinMode(9,INPUT);
+  pinMode(8,OUTPUT);
+  //pinMode(3,OUTPUT);
+  Serial.begin(9600); //initialise serial monitor
+
+}
+
+void loop() {
+//  int temp=digitalRead(7);      //assign value of LDR sensor to a temporary variable
+//  Serial.println("Intensity="); //print on serial monitor using ""
+//  Serial.println(temp);         //display output on serial monitor
+//  delay(300);
+//  digitalWrite(8,HIGH);
+//  delay(300);
+//  digitalWrite(8,LOW);
+////  if(temp==HIGH) 
+////  {//HIGH means,light got blocked
+////  digitalWrite(8,HIGH);
+////  //digitalWrite(3,HIGH);
+////  }//if light is not present,LED on
+////  else
+////  {
+////  digitalWrite(8,LOW);   
+////  //digitalWrite(3,LOW);
+////  }//if light is present,LED off
+
+}
